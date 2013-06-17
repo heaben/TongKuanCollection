871d20e5213abebadd3a3bb4f95581ea5ac5b19b
heaben <hb91@qq.com>
2013-06-17 09:45:09 +0800

    Commit

---
+++ TongKuan/TongKuanViewController.m
@@ -0,0 +1,29 @@
+//
+//  TongKuanViewController.m
+//  TongKuan
+//
+//  Created by Beny on 13-5-21.
+//  Copyright (c) 2013年 TongKuan. All rights reserved.
+//
+
+#import "TongKuanViewController.h"
+
+@interface TongKuanViewController ()
+
+@end
+
+@implementation TongKuanViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
